847e7af43189d3014a76883e9401ff2d68c5d74b
Der Ketzer <der.ketzer@gmail.com>
2013-11-08 18:45:39 -0600

    Se mejoró el script de deploy y ya funciona bonito =)

--- cm_script.sh
+++ cm_script.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 
 DEBUG=""
+ENV="dev"
 
-if [ $# -eq 0 ]
-then
-	ENV="dev"
-else
-	ENV = $1
-	
-	if [ $1 -eq "prod" ]
-	then
-		DEBUG=" --no-debug"
-	fi
+PATH_LOCAL="/Users/der_ketzer/Arbeit/Freelance/Codemonkey/"
+PATH_REMOTE="/home1/ketzer/symfony/codemonkey/v2/"
+
+roption=(
+	-av
+	-e "ssh"
+	--exclude=".DS_Store"
+	--exclude=".git/"
+	--exclude=".idea/"
+	--exclude=".gitignore"
+	--exclude="cm_script.sh"
+)
+
+if [ $1 ]; then
+    if [ $1 = "prod" ] || [ $1 = "deploy" ]; then
+            ENV="prod"
+            DEBUG="--no-debug"
+    fi
+
+    if [ $1 = "deploy" ]; then
+        printf "\n== Updating vendors == \n"
+        php composer.phar install --no-dev --optimize-autoloader
+    fi
+
+    if [ $1 = "prod" ] || [ $1 = "deploy" ]; then
+        php app/console cache:clear --env=dev
+    fi
 fi
 
-php /Users/derketzer/Sites/symfony/app/console cache:clear --env=$ENV$DEBUG
-php /Users/derketzer/Sites/symfony/app/console assets:install --env=$ENV$DEBUG /Users/derketzer/Sites/symfony/web/
-php /Users/derketzer/Sites/symfony/app/console assetic:dump --env=$ENV$DEBUG /Users/derketzer/Sites/symfony/web/
+php app/console cache:clear --env=$ENV
+php app/console assets:install --env=$ENV $DEBUG web/
+php app/console assetic:dump --env=$ENV $DEBUG web/
+
+if [ $1 ]; then
+    if [ $1 = "deploy" ]; then
+        printf "\n== Deploying to Production ==\n"
+        rsync "${roption[@]}" $PATH_LOCAL hostgator:$PATH_REMOTE
+    fi
+fi
